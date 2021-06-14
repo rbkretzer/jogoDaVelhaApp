0a3a1369ab143fe5a5ec9f608c8050bba5c0bb99
JoshuaAlves-code <jpallves@furb.br>
2021-06-14 18:58:22 -0300

    Tomara que dê certo

--- src/application/JogoDaVelha.java
+++ src/application/JogoDaVelha.java
@@ -4,7 +4,7 @@ public class JogoDaVelha {
 
 	public static void main(String[] args) {
 		
-		System.out.println("teste para ver se está indo tudo certo");
+		System.out.println("Teste para ver se está indo tudo certo :)");
 		int teste1 = 0;
 		int teste2 = 1;
 		int teste3 = 2;
