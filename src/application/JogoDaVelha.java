4e6ea75960d23e4c1745f5bcc40cf61b87d90ea7
Pedroschumann <pedroschumann2016@gmail.com>
2021-06-14 19:13:33 -0300

    Teste do pedro

--- src/application/JogoDaVelha.java
+++ src/application/JogoDaVelha.java
@@ -5,9 +5,9 @@ public class JogoDaVelha {
 	public static void main(String[] args) {
 		
 		System.out.println("Teste para ver se está indo tudo certo :)");
-		int teste1 = 0;
-		int teste2 = 1;
-		int teste3 = 2;
+		int teste1 = 10;
+		int teste2 = 6;
+		int teste3 = 5;
 
 		System.out.print(teste1 + ", " + teste2 + ", " + teste3 + ".");
 	}
